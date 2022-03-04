d36b07b8dc64fd6017e9f79b9dc5bb16145685df
Seogun95 <ksj083895@gmail.com>
2022-03-05 04:45:30 +0500

    Initial Commit

---
+++ Shared/SwiftUI_FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FruitsApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
